8414a30d6cc29b640ac55c33b6dc94ba3e56494b
Jonah Dahlquist <jonah@nucleussystems.com>
2015-03-18 10:54:33 -0700

    Changed connection port, began adapting connection process

--- conf/default.js
+++ conf/default.js
@@ -26,7 +26,7 @@ module.exports = {
 			user: 'root',
 			password: '',
 			host: 'localhost',
-			port: 3306
+			port: 5432
 		}
 	}
 };
--- conf/example.config.js
+++ conf/example.config.js
@@ -8,7 +8,7 @@ module.exports = {
 			user: 'root',
 			password: '',
 			host: 'localhost',
-			port: 3306,
+			port: 5432,
 
 			generateModelsFromSchema: true
 		}
